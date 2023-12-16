40a9ecf85f24e0b3f166954b0e9b7a45387f3058
evanryuu <evanryuu@outlook.com>
2023-12-16 12:40:09 +0800

    feat: solution 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="main">
+    <main className="main bg-white dark:bg-black text-black dark:text-white">
       <button className="btn">Toggle</button>
     </main>
   )
